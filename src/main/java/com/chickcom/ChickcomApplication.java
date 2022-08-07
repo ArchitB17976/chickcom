d63c07d62f990110c7381ca5666a8ad2d264597c
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-08 00:35:15 +0800

    Project init

---
+++ src/main/java/com/chickcom/ChickcomApplication.java
@@ -0,0 +1,13 @@
+package com.chickcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chickcom/ChickcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chickcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
